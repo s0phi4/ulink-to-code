169783f38d57a5e253e0cdc6f94f39efc064ff93
yourUsername <your_email@example.com>
2017-02-19 14:05:51 -0500

    added div to html for handlebars

--- assets/scripts/links/ui_link.js
+++ assets/scripts/links/ui_link.js
@@ -5,7 +5,8 @@ const successCreateGame = () => {
   $('h2').hide();
 };
 
-const showGamesTotal = (data) => {
+const showUserResources = function(data) {
+let resourceTemplate = require('./templates/user-resource.handlebars');
 $('h2').show();
 $('h2').text('You killed time '+ data.games.length + ' times!');
 };
@@ -13,6 +14,6 @@ $('h2').text('You killed time '+ data.games.length + ' times!');
 
 
 module.exports = {
-  showGamesTotal,
+  showUserResources,
   successCreateGame,
 };
