4c232953b0f7946aa19f61368273e495d31f2e65
yourUsername <your_email@example.com>
2017-02-19 23:33:14 -0500

    Created resource handlebar file Modified Html divs added css

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -10,15 +10,17 @@ $(() => {
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example');
-const authEvents = require('./auth/events.js');
+
+// const authEvents = require('./auth/events.js');
 // const events_links = require('./links/events_links.js');
+
 // use require without a reference to ensure a file is bundled
 require('./example');
 
 // On document ready
 $(() => {
 
-  authEvents.addHandlers();
+  // authEvents.addHandlers();
   // events_links.linkHandlers();
 
   $('a[rel=popover]').popover({
